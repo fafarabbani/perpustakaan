1982290255c558fc8c356b075b57b0f9739a92b2
FafaRabbani <fafa.rabbanii@gmail.com>
2025-10-26 14:31:08 +0700

    pertama commit

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,7 +4,7 @@
             <h2 class="text-xl font-semibold leading-tight">
                 {{ __('Dashboard Admin') }}
             </h2>
-            <x-button target="_blank" href="https://github.com/kamona-wd/kui-laravel-breeze" variant="black"
+            <x-button target="_blank" href="https://github.com/kamona-wd/perpustakaan" variant="black"
                 class="justify-center max-w-xs gap-2">
                 <x-icons.github class="w-6 h-6" aria-hidden="true" />
                 <span>Star on Github</span>
